bb892d5d553d8bf1837d6a5ae0ceba6dbc624039
Ben slama Taieb <taiebbenslama951@gmail.com>
2025-03-13 14:04:12 +0100

    pushing changes

--- hello-devops/app.py
+++ hello-devops/app.py
@@ -4,5 +4,9 @@ app = Flask ( __name__ )
 @app . route ('/')
 def hello ():
     return "Hello , CI/CD!"
+@app . route ('/status')
+def status ():
+    return "OK"
+
 if __name__ == " __main__ ":
     app . run ()
